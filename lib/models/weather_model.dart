8193129c973e5e9850f64c9300d5ce93378e694e
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-04-27 18:23:32 +0300

    Add Weather.fromJson factory method to weather_model.dart

--- lib/models/weather_model.dart
+++ lib/models/weather_model.dart
@@ -8,4 +8,12 @@ class Weather {
     required this.temperature,
     required this.mainCondition,
   });
+
+  factory Weather.fromJson(Map<String, dynamic> json) {
+    return Weather(
+      cityName: json['name'],
+      temperature: json['main']['temp'].toDouble(),
+      mainCondition: json['weather'][0]['main'],
+    );
+  }
 }
