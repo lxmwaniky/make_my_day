4af4f6fb2f3277a92bfb78303bb2f4bce301459a
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-04-27 17:39:51 +0300

    Refactor MaterialApp widget to use const constructor in main.dart

---
+++ lib/models/weather_model.dart
@@ -0,0 +1,11 @@
+class Weather {
+  final String cityName;
+  final double temperature;
+  final String mainCondition;
+
+  Weather({
+    required this.cityName,
+    required this.temperature,
+    required this.mainCondition,
+  });
+}
