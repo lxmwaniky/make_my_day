a063ac738ff8affe7dc07f4281700409014d7029
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-04-27 17:19:41 +0300

    Add geolocator and geocoding dependencies, and register geolocator plugin in macOS and Windows

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:make_my_day/pages/home_page.dart';
+import 'package:make_my_day/pages/weather_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: WeatherPage(),
     );
   }
 }
